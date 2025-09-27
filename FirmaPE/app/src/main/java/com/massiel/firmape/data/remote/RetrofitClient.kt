27afd56552e2380182d03cf9669c64d4e5c69583
Alev27 <alejandrocamayteri2019@gmail.com>
2025-09-27 16:11:01 -0500

    Fix: acceso api de servicios

--- FirmaPE/app/src/main/java/com/massiel/firmape/data/remote/RetrofitClient.kt
+++ FirmaPE/app/src/main/java/com/massiel/firmape/data/remote/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://<tu-usuario>.github.io/<tu-repo>/"
+    private const val BASE_URL = "https://alevv27.github.io/massiel-api/"
 
     fun api(): ApiService {
         val log = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
